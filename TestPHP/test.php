6bfde673a3be736cbc93b26b6a3004dc797fce52
PetrKremenak <vanishak00@gmail.com>
2026-02-13 08:36:22 +0100

    DRUHY_TEST

--- TestPHP/test.php
+++ TestPHP/test.php
@@ -1,5 +1,19 @@
 <?php
+$name = "";
+$age = 0;
+$message = "";
 
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $name = $_POST["my_name"];
+    if($name == "Petr"){
+        $message = "CC";
+    }
+    else{
+        $message = "kys";
+    }
+    $age = $_POST["age"];
+    
+}
 
 
 
@@ -16,5 +30,20 @@
 </head>
 <body>
     <h1>Test Formuláře</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime consequuntur nulla iure quia reprehenderit impedit asperiores fugit officiis placeat. Sit ratione omnis accusamus amet hic suscipit. Unde odit nisi perferendis.
+    </p>
+    <form method="post">
+        <input type="text", name="my_name", placeholder="Zadejte Jméno">
+        <input type="number", name="age", placeholder="Zadejte Váš Věk">
+        <button type="submit">Odeslat</button>
+    </form>
+    <p>
+        <?php 
+        echo $message . " "; // Added space and semicolon
+        echo "je Vám ";      // Added space and semicolon
+        echo $age; 
+    ?>
+        
+    </p>
 </body>
 </html>
